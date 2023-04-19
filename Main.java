934428ffd695a23ddf194a265465cfb90b19c6d0
kokon02 <127319252+kokon02@users.noreply.github.com>
2023-04-19 13:28:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int wybor;
+    System.out.println("1.Wprowadz nowego studenta");
+    
+    wybor = scan.nextInt();
+    
+    switch (wybor){
+      case 1:
+        System.out.println("Podaj imie:");
+        String name = scan.next();
+        System.out.println("Podaj wiek:");
+          Int wiek = scan.nextInt();
+        try{
+          Service s = new Service();
+          
+          s.addStudent(new Student(Imie, Wiek));
+
+          var students = s.getStudents();
+          
+          
+          for (Student current : students){
+            
+            System.out.println(current.ToString());
+            
+          }
+          catch (IOException e){
+              
+            }
+          
+        } 
+        
+        
+    }
+    
+    }  
+
+    
+  
+}
